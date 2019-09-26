866ea0db8209d7bba0c1eb41a3a03b9d59d15a00
NaveenTennakoon <naveentennakoon27@gmail.com>
2019-09-26 22:39:09 +0530

    Initial files

---
+++ main.cpp
@@ -0,0 +1,113 @@
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// includes for gradeutil
+#include "gradeutil.h"
+
+College InputGradeData(string filename)
+{
+    College college;
+    ifstream file(filename);
+    string line, value;
+
+    if (!file.good())
+    {
+        cout << "**Error: unable to open input file '" << filename << "'." << endl;
+        return college;
+    }
+
+    // first line contains semester,year
+    getline(file, line);
+    stringstream ss(line);
+
+    getline(ss, college.Name, ',');
+    getline(ss, college.Semester, ',');
+    getline(ss, value);
+    college.Year = stoi(value);
+
+    // second line contains column headers --- skip
+    getline(file, line);
+
+    //
+    // now start inputting and parse course data:
+    //
+
+    while (getline(file, line))
+    {
+        Course c = ParseCourse(line);
+
+        //
+        // search for correct dept to ask course to, otherwise create a new dept:
+        //
+        auto dept_iter = std::find_if(college.Depts.begin(),
+                                      college.Depts.end(),
+                                      [&](const Dept &d) {
+                                          return (d.Name == c.Dept);
+                                      });
+
+        if (dept_iter == college.Depts.end())
+        {
+            //
+            // doesn't exist, so we have to create a new dept
+            // and insert course:
+            //
+            Dept d(c.Dept);
+
+            d.Courses.push_back(c);
+
+            college.Depts.push_back(d);
+        }
+        else
+        {
+            // dept exists, so insert course into existing dept:
+            dept_iter->Courses.push_back(c);
+        }
+
+    } //while
+
+    //
+    // done:
+    //
+    return college;
+}
+
+// TODO: define your own functions
+//
+//
+
+int main()
+{
+    string filename;
+
+    cout << std::fixed;
+    cout << std::setprecision(2);
+
+    //
+    // 1. Input the filename and then the grade data:
+    //
+    cin >> filename;
+    // filename = "fall-2018.csv";
+
+    College college = InputGradeData(filename);
+
+    // 2. TODO: print out summary of the college
+    // DEFINE your own functions
+
+    //
+    // 3. TODO: Start executing commands from the user:
+    // DEFINE your own functions
+    
+
+    //
+    // done:
+    //
+    return 0;
+}
